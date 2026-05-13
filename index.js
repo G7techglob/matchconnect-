69ca4323c6956ab02bbbb4409fc74658f0d1284f
tpao66 <tpaopetroleum@gmail.com>
2026-05-13 03:59:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,96 @@
 const profiles = [
+
   {
-    name: "Sophia",
-    age: 24,
-    city: "Tripoli",
-    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=800",
-    video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    name:"Sophia",
+    age:24,
+    city:"Tripoli",
+
+    image:
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=800",
+
+    video:
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
   },
 
   {
-    name: "Daniel",
-    age: 27,
-    city: "Benghazi",
-    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=800",
-    video: "https://www.youtube.com/watch?v=ysz5S6PUM-U"
+    name:"Daniel",
+    age:27,
+    city:"Benghazi",
+
+    image:
+    "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=800",
+
+    video:
+    "https://www.youtube.com/watch?v=ysz5S6PUM-U"
   },
 
   {
-    name: "Amara",
-    age: 22,
-    city: "Misrata",
-    image: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?q=80&w=800",
-    video: "https://www.youtube.com/watch?v=jNQXAC9IVRw"
+    name:"Amara",
+    age:22,
+    city:"Misrata",
+
+    image:
+    "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?q=80&w=800",
+
+    video:
+    "https://www.youtube.com/watch?v=jNQXAC9IVRw"
   }
+
 ];
 
-const profilesContainer = document.getElementById("profiles");
+
+
+const profilesContainer =
+document.getElementById("profiles");
+
+
 
 profiles.forEach(profile => {
 
   profilesContainer.innerHTML += `
-  
-    <div class="profile-card">
 
-      <img src="${profile.image}" alt="${profile.name}">
+  <div class="profile-card">
 
-      <div class="profile-content">
+    <img src="${profile.image}">
 
-        <h3>${profile.name}, ${profile.age}</h3>
+    <div class="profile-content">
 
-        <p>📍 ${profile.city}</p>
+      <h3>
+        ${profile.name},
+        ${profile.age}
+      </h3>
 
-        <div class="profile-buttons">
+      <p>
+        📍 ${profile.city}
+      </p>
 
-          <a href="${profile.image}" target="_blank" class="photo-btn">
-            View Photo
-          </a>
+      <div class="profile-buttons">
 
-          <a href="${profile.video}" target="_blank" class="video-btn">
-            Watch Video
-          </a>
+        <a
+        href="${profile.image}"
+        target="_blank"
+        class="photo-btn">
 
-        </div>
+          View Photo
+
+        </a>
+
+
+        <a
+        href="${profile.video}"
+        target="_blank"
+        class="video-btn">
+
+          Watch Video
+
+        </a>
 
       </div>
 
     </div>
 
+  </div>
+
   `;
+
 });
