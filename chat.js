b636eabeb3eebeb3acad84b238d610ece1f2da4c
G7techglob <tpaopetroleum@gmail.com>
2026-06-05 23:22:01 +0200

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,54 @@
+import { auth, db } from "./firebase.js";
+import {
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    onSnapshot,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const messagesDiv = document.getElementById("messages");
+const sendBtn = document.getElementById("sendBtn");
+const input = document.getElementById("messageInput");
+
+// SEND MESSAGE
+sendBtn.addEventListener("click", async () => {
+    const text = input.value;
+
+    if (text.trim() === "") return;
+
+    await addDoc(collection(db, "messages"), {
+        text: text,
+        user: auth.currentUser.email,
+        time: serverTimestamp()
+    });
+
+    input.value = "";
+});
+
+// REALTIME MESSAGES
+const q = query(collection(db, "messages"), orderBy("time"));
+
+onSnapshot(q, (snapshot) => {
+    messagesDiv.innerHTML = "";
+
+    snapshot.forEach((doc) => {
+        const msg = doc.data();
+
+        const div = document.createElement("div");
+        div.innerHTML = `<b>${msg.user}</b>: ${msg.text}`;
+        messagesDiv.appendChild(div);
+    });
+});
+
+// LOGOUT
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    signOut(auth).then(() => {
+        window.location.href = "login.html";
+    });
+});
