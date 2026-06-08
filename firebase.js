445350b6b2632012e979b687bc597eec00c46ca7
G7techglob <tpaopetroleum@gmail.com>
2026-06-08 02:59:36 +0200

    Export auth instance from firebase.js

--- firebase.js
+++ firebase.js
@@ -14,7 +14,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+export const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
 
