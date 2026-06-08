2bb137605e9c98095247880f9a1960f95fb9b5e5
G7techglob <tpaopetroleum@gmail.com>
2026-06-08 21:01:39 +0200

    Update posts.js

--- posts.js
+++ posts.js
@@ -8,6 +8,9 @@ import {
   query,
   orderBy,
   serverTimestamp
+  doc,
+updateDoc,
+increment
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 import { auth } from "./firebase.js";
