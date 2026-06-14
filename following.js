5b6cb449a35c2b2d11643c49c5ff20b5e6d8c591
G7techglob <tpaopetroleum@gmail.com>
2026-06-14 06:35:23 +0200

    Implement Firebase following list retrieval
    This code initializes Firebase, retrieves the following list for a user, and displays it in the UI.

--- following.js
+++ following.js
@@ -1,1 +1,111 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc
+}
+from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVdy9nJLp3YDV9PNB9kfR3HiQCdFdvGmg",
+  authDomain: "matchconnect-44a3e.firebaseapp.com",
+  projectId: "matchconnect-44a3e",
+  storageBucket: "matchconnect-44a3e.firebasestorage.app",
+  messagingSenderId: "283382943870",
+  appId: "1:283382943870:web:ee1d08c65bcbac400cc82f"
+};
+
+const app =
+  initializeApp(firebaseConfig);
+
+const db =
+  getFirestore(app);
+
+const params =
+  new URLSearchParams(
+    window.location.search
+  );
+
+const uid =
+  params.get("uid");
+
+const container =
+  document.getElementById(
+    "followingList"
+  );
+
+container.innerHTML = "";
+
+const followingSnapshot =
+  await getDocs(
+    collection(
+      db,
+      "users",
+      uid,
+      "following"
+    )
+  );
+
+document.getElementById(
+  "followingTotal"
+).textContent =
+  followingSnapshot.size;
+
+if (
+  followingSnapshot.empty
+) {
+
+  container.innerHTML =
+    "Not following anyone yet";
+
+} else {
+
+  followingSnapshot.forEach(
+    async (followingDoc) => {
+
+      const followingId =
+        followingDoc.id;
+
+      const userDoc =
+        await getDoc(
+          doc(
+            db,
+            "users",
+            followingId
+          )
+        );
+
+      if (
+        userDoc.exists()
+      ) {
+
+        const userData =
+          userDoc.data();
+
+        const div =
+          document.createElement(
+            "div"
+          );
+
+        div.innerHTML = `
+          <p>
+            <a href="user.html?uid=${followingId}">
+              ${userData.name || "User"}
+            </a>
+          </p>
+        `;
+
+        container.appendChild(
+          div
+        );
+
+      }
+
+    }
+  );
+
+    }
