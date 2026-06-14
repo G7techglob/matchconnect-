34351101e4515388b0eabb2549d624c8794d31e6
G7techglob <tpaopetroleum@gmail.com>
2026-06-14 09:02:34 +0200

    Add console log for profile.js loading

--- profile.js
+++ profile.js
@@ -35,6 +35,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const db = getFirestore(app);
+console.log("PROFILE JS LOADED");
 
 auth.onAuthStateChanged(
   async (user) => {
