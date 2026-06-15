d2bbcadd7ddff74ec22f07a38918fe6b36b2be2b
G7techglob <tpaopetroleum@gmail.com>
2026-06-15 03:01:44 +0200

    Refactor comment loading in profile.js

--- profile.js
+++ profile.js
@@ -343,9 +343,41 @@ for (const postDoc of postsSnapshot.docs) {
 <hr>
 `;
 
-    myPosts.appendChild(div);
+myPosts.appendChild(div);
 
-  });
+const commentsContainer =
+  div.querySelector(
+    `#comments-${postDoc.id}`
+  );
+
+const commentsSnapshot =
+  await getDocs(
+    collection(
+      db,
+      "posts",
+      postDoc.id,
+      "comments"
+    )
+  );
+
+commentsSnapshot.forEach(
+  (commentDoc) => {
+
+    const comment =
+      commentDoc.data();
+
+    const p =
+      document.createElement("p");
+
+    p.innerHTML =
+      `<strong>${comment.username}</strong>: ${comment.text}`;
+
+    commentsContainer.appendChild(p);
+
+  }
+);
+
+});
 
 }
 
