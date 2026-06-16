c19f5d9f882201b02fbbe13fa0e8357d6c1722e1
G7techglob <tpaopetroleum@gmail.com>
2026-06-16 19:10:53 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -11,14 +11,15 @@ import {
     onSnapshot,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const messagesDiv =
   document.getElementById(
-    "messagesContainer"
+    "messages"
   );
 
 const sendBtn =
   document.getElementById(
-    "sendMessageBtn"
+    "sendBtn"
   );
 
 const input = document.getElementById("messageInput");
