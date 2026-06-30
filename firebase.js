4b78c21bb1646cd75af87caa6da191f7d784c861
G7techglob <globalinfortv24@gmail.com>
2026-06-30 18:27:47 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,33 +4,31 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCVdy9nJLp3YDV9PNB9kfR3HiQCdFdvGmg",
   authDomain: "matchconnect-44a3e.firebaseapp.com",
   projectId: "matchconnect-44a3e",
-  storageBucket: "matchconnect-44a3e.firebasestorage.app",
+  storageBucket: "matchconnect-44a3e.appspot.com",
   messagingSenderId: "283382943870",
   appId: "1:283382943870:web:ee1d08c65bcbac400cc82f"
 };
 
 const app = initializeApp(firebaseConfig);
 
-const storage = getStorage(app);
-window.storage = storage;
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export { app };
+const storage = getStorage(app);
+
+window.auth = auth;
+window.db = db;
+window.storage = storage;
 
 onAuthStateChanged(auth, (user) => {
-
   if (user) {
     console.log("Logged in:", user.email);
   } else {
     console.log("No user logged in");
   }
-
 });
