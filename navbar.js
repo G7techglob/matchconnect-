3d04caa7c0216ec9648105364a83fc143276d018
G7techglob <globalinfortv24@gmail.com>
2026-07-01 15:42:16 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", async () => {
-
     const container = document.getElementById("navbar-container");
+
     if (!container) return;
 
-    const basePath = window.location.pathname
-        .includes("/")
-        ? ""
-        : "";
-
     try {
-        const response = await fetch(basePath + "navbar.html");
+        const response = await fetch("./navbar.html", {
+            cache: "no-store"
+        });
+
         const html = await response.text();
         container.innerHTML = html;
+
     } catch (err) {
         console.log("Navbar load error:", err);
     }
-
 });
