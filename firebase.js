21f5fbaad5e092660d5ff09b8477c2806c15d88f
G7techglob <globalinfortv24@gmail.com>
2026-07-01 06:48:29 +0200

    Add signout to Firebase authentication imports

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import {
   getAuth,
-  onAuthStateChanged
+  onAuthStateChanged,
+  signout
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
