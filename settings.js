2f1c712d90e2317489e7a8a4ce7714ad4e19ea7a
G7techglob <globalinfortv24@gmail.com>
2026-07-04 19:22:12 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,106 @@
+import { auth, db } from "../firebase.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+
+const profileImage = document.getElementById("profileImage");
+const displayName = document.getElementById("displayName");
+const email = document.getElementById("email");
+
+const logoutBtn = document.getElementById("logoutBtn");
+const backBtn = document.getElementById("backBtn");
+
+
+
+/* -----------------------------
+   LOAD USER
+-------------------------------- */
+
+onAuthStateChanged(auth, async (user)=>{
+
+    if(!user){
+
+        window.location.href="login.html";
+        return;
+
+    }
+
+    try{
+
+        const docRef = doc(db,"users",user.uid);
+
+        const docSnap = await getDoc(docRef);
+
+        if(docSnap.exists()){
+
+            const data = docSnap.data();
+
+            displayName.textContent =
+                data.username || "Unknown User";
+
+            email.textContent =
+                data.email || user.email;
+
+            if(data.photoURL){
+
+                profileImage.src =
+                    data.photoURL;
+
+            }
+
+        }
+
+    }catch(error){
+
+        console.error(error);
+
+    }
+
+});
+
+
+/* -----------------------------
+   BACK BUTTON
+-------------------------------- */
+
+backBtn.addEventListener("click",()=>{
+
+    history.back();
+
+});
+
+
+/* -----------------------------
+   LOGOUT
+-------------------------------- */
+
+logoutBtn.addEventListener("click",async()=>{
+
+    const confirmLogout =
+        confirm("Do you want to log out?");
+
+    if(!confirmLogout) return;
+
+    try{
+
+        await signOut(auth);
+
+        window.location.href="login.html";
+
+    }
+
+    catch(error){
+
+        alert(error.message);
+
+    }
+
+});
