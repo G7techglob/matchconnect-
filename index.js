b7a766e0cbd96cc83137b24ff26f792de51db74b
G7techglob <globalinfortv24@gmail.com>
2026-07-13 20:07:54 +0200

    Remove service worker event listeners
    Removed service worker event listeners for install and fetch.

--- index.js
+++ index.js
@@ -140,17 +140,6 @@ if ("serviceWorker" in navigator) {
       .then(() => console.log("Service Worker Registered"));
   });
 }
-
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
-});
-
-
-
 let currentAd = 0;
 
 function loadAd() {
