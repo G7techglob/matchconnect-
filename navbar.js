690ed3c3040581809e4da21a54c8187549b131f1
G7techglob <globalinfortv24@gmail.com>
2026-07-17 12:09:58 +0200

    Refactor navbar.js to remove unused imports and code
    Removed Firebase authentication and Firestore logic from navbar.js.

--- navbar.js
+++ navbar.js
@@ -1,18 +1,3 @@
-
-
-import { auth, db } from "./firebase.js";
-
-import {
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import {
-  doc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-
-alert("navbar.js is running");
-
 document.addEventListener("DOMContentLoaded", async () => {
 
     const container = document.getElementById("navbar-container");
@@ -37,36 +22,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         console.log("Navbar loaded successfully");
 
-
-        // Wait until navbar exists
-        const navProfileImage =
-        document.getElementById("navProfileImage");
-
-
-        onAuthStateChanged(auth, async (user)=>{
-
-            if(user && navProfileImage){
-
-                const userRef = doc(db,"users",user.uid);
-
-                const snap = await getDoc(userRef);
-
-
-                if(snap.exists()){
-
-                    const data = snap.data();
-
-                    navProfileImage.src =
-                    data.photoURL || "default-avatar.png";
-
-                }
-
-            }
-
-        });
-
-
-    } catch(err){
+    } catch (err) {
 
         console.log("Navbar error:", err);
 
