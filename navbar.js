c9333cfb578d0b00475e7b262675c1c08ed2ef5e
G7techglob <globalinfortv24@gmail.com>
2026-07-17 10:07:44 +0200

    Display user profile image in navbar
    Added user profile image to navbar based on authentication state.

--- navbar.js
+++ navbar.js
@@ -1,3 +1,14 @@
+
+import { auth, db } from "./firebase.js";
+
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 document.addEventListener("DOMContentLoaded", async () => {
 
     const container = document.getElementById("navbar-container");
@@ -24,5 +35,28 @@ document.addEventListener("DOMContentLoaded", async () => {
     } catch (err) {
         console.log("Navbar error:", err);
     }
+
+});
+
+const navProfileImage = document.getElementById("navProfileImage");
+
+onAuthStateChanged(auth, async (user)=>{
+
+  if(user){
+
+    const userRef = doc(db,"users",user.uid);
+
+    const snap = await getDoc(userRef);
+
+    if(snap.exists()){
+
+      const data = snap.data();
+
+      navProfileImage.src =
+        data.photoURL || "default-avatar.png";
+
+    }
+
+  }
 
 });
