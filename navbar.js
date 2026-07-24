88fe3cc9f5feb0e9f2322c6c34893106193563c0
G7techglob <globalinfortv24@gmail.com>
2026-07-25 01:37:38 +0200

    Import Firebase modules in navbar.js

--- navbar.js
+++ navbar.js
@@ -1,3 +1,14 @@
+import { auth, db } from "./firebase.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
 document.addEventListener("DOMContentLoaded", async () => {
 
     const container = document.getElementById("navbar-container");
