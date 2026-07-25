0af072b080a4e1ea62004657f02ea66b2ae930ee
G7techglob <globalinfortv24@gmail.com>
2026-07-25 13:50:46 +0200

    Enhance login process with email verification
    Added email verification check after login.

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
+import {
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 const loginBtn = document.getElementById("loginBtn");
 
 loginBtn.addEventListener("click", () => {
@@ -9,10 +11,25 @@ loginBtn.addEventListener("click", () => {
     const password = document.getElementById("loginPassword").value;
 
     signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-        alert("Login successful!");
-        window.location.href = "index.html";
-    })
+    .then(async (userCredential) => {
+
+    const user = userCredential.user;
+
+    // Refresh the user's information
+    await user.reload();
+
+    if (!user.emailVerified) {
+        alert("Please verify your email before logging in.");
+
+        await signOut(auth);
+
+        return;
+    }
+
+    alert("Login successful!");
+
+    window.location.href = "index.html";
+})
     .catch((error) => {
         alert(error.message);
     });
