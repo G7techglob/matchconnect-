78074c4cb1a35c648ece8a11eeaab34ca3e4624e
G7techglob <globalinfortv24@gmail.com>
2026-08-05 18:08:09 +0200

    Add inbox settings JavaScript functionality

---
+++ inbox-settings.js
@@ -0,0 +1,33 @@
+import { auth } from "./firebase.js";
+
+document.getElementById("backBtn").onclick = () => {
+    history.back();
+};
+
+document.getElementById("viewProfile").onclick = () => {
+    alert("Coming soon");
+};
+
+document.getElementById("muteChat").onclick = () => {
+    alert("Coming soon");
+};
+
+document.getElementById("pinChat").onclick = () => {
+    alert("Coming soon");
+};
+
+document.getElementById("clearChat").onclick = () => {
+    alert("Coming soon");
+};
+
+document.getElementById("deleteChat").onclick = () => {
+    alert("Coming soon");
+};
+
+document.getElementById("blockUser").onclick = () => {
+    alert("Coming soon");
+};
+
+document.getElementById("reportUser").onclick = () => {
+    alert("Coming soon");
+};
