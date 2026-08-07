610c76042732865cf5066483de27e166ce9db7c5
G7techglob <globalinfortv24@gmail.com>
2026-08-07 18:24:35 +0200

    Update service worker cache version and cleanup
    Updated cache name to version 2 and added activation event to clear old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "matchconnect-v1";
+const CACHE_NAME = "matchconnect-v2";
 
 const urlsToCache = [
   "./",
@@ -18,5 +18,19 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
   );
 });
