20a46d903512153703ce46a16dc53eada2e85f33
G7techglob <globalinfortv24@gmail.com>
2026-08-08 19:23:27 +0200

    Enable Firebase Auth persistence
    Added Firebase Auth persistence using browserLocalPersistence.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import {
   getAuth,
+  setPersistence,
+  browserLocalPersistence,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
@@ -19,6 +20,14 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    console.log("Firebase Auth persistence enabled");
+  })
+  .catch((error) => {
+    console.error("Auth persistence error:", error);
+  });
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
